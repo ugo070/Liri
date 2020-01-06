37e7b15d50c05a7fcc79ca14c56b48a517d51934
ugo070 <ugo070@yahoo.com>
2020-01-05 19:06:23 -0500

    amendment

--- keys.js
+++ keys.js
@@ -1,11 +1,6 @@
-//console.log('this is loaded');
+console.log("This is loaded!");
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-};
-
-// exports.seatgeek = {
-//   id: process.env.SEATGEEK_ID,
-//   secret: process.env.SEATGEEK_SECRET
-// };
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+}
--- liri.js
+++ liri.js
@@ -1,109 +1,111 @@
-const axios = require("axios");
 require("dotenv").config();
+
 var keys = require("./keys.js");
-const moment = require("moment");
-const fs = require("fs");
-var Spotify = require("node-spotify-api");
-let searchTermHyphen = process.argv.slice(3).join("-");
-let searchTermSpace = process.argv.slice(3).join(" ");
+var Spotify = require('node-spotify-api'); //Using the Spotify api and getting the key from keys.js
 var spotify = new Spotify(keys.spotify);
-// var seatgeek = keys.seatgeek.id;
 
-// if (process.argv[2] === "concert-this") {
-//   concertThis(searchTermHyphen);
-// }
-// function concertThis(searchTermHyphen) {
-//   const URL = `https://api.seatgeek.com/2/events?client_id=${seatgeek}&performers.slug=${searchTermHyphen}`;
-//   axios.get(URL).then(function(response) {
-//     const answer = response.data.events[0];
-//     console.log(answer.venue.name);
-//     console.log(answer.venue.extended_address);
-//     //formatting date with moment
-//     console.log(moment(answer.datetime_local).format("MM/DD/YYYY"));
-//   });
-// }
+var moment = require('moment'); //Both required to use moment for node
+moment().format();
 
-if (process.argv[2] === "spotify-this-song") {
-  spotifyThisSong(searchTermSpace);
-}
-function spotifyThisSong(searchTermSpace) {
-  // setting search parameter to These Are Special Times of base if undeclared
-  if (!searchTermSpace) {
-    searchTermSpace = "These Are Special Times";
-  }
+var axios = require('axios'); //To get the information from the APIs for movie and concert-this
 
-  spotify.search({ type: "track", query: searchTermSpace }, function(
-    err,
-    data
-  ) {
-    // (below) if error redo search with the These Are Special Times as search param
-    if (err) {
-      spotify.search(
-        { type: "track", query: "These Are Special Times" },
-        function(err, data) {
-          if (err) {
-            return console.log("Error occurred: " + err);
-          } else {
-            const answer = data.tracks.items[0];
-            console.log(answer.artists[i].name);
-            console.log(answer.name);
-            console.log(answer.external_urls.spotify);
-            console.log(answer.album.name);
-          }
+var fs = require('fs'); //To read the random.txt file for the do-what-it-says function
+
+var command = process.argv[2]; //For the switch statement
+var value = process.argv[3]; //To send the song/movie/concert to their respective functions
+
+switch (command) {
+    case "concert-this":
+        concertThis(value);
+        break;
+    case "spotify-this-song":
+        spotifySong(value);
+        break;
+    case "movie-this":
+        movieThis(value);
+        break;
+    case "do-what-it-says":
+        doThis(value);
+        break;
+};
+
+function concertThis(value) {
+    axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp")
+    .then(function(response) {    
+        for (var i = 0; i < response.data.length; i++) {
+
+            var datetime = response.data[i].datetime; //Saves datetime response into a variable
+            var dateArr = datetime.split('T'); //Attempting to split the date and time in the response
+
+            var concertResults = 
+                "--------------------------------------------------------------------" +
+                    "\nVenue Name: " + response.data[i].venue.name + 
+                    "\nVenue Location: " + response.data[i].venue.city +
+                    "\nDate of the Event: " + moment(dateArr[0], "MM-DD-YYYY"); //dateArr[0] should be the date separated from the time
+            console.log(concertResults);
         }
-      );
-      return console.log("Error occurred: " + err);
-    }
-    // (above) if error redo search with These Are Special Times as search param
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+        
 
-    const answer = data.tracks.items[0];
-
-    //looping through multiple artists if they exist
-    for (i = 0; i < answer.artists.length; i++) {
-      console.log(answer.artists[i].name);
-    }
-
-    console.log(answer.name);
-    console.log(answer.external_urls.spotify);
-    console.log(answer.album.name);
-  });
 }
 
-if (process.argv[2] === "movie-this") {
-  movieThis(searchTermHyphen);
+function spotifySong(value) {
+    if(!value){
+        value = "The Sign";
+    }
+    spotify
+    .search({ type: 'track', query: value })
+    .then(function(response) {
+        for (var i = 0; i < 5; i++) {
+            var spotifyResults = 
+                "--------------------------------------------------------------------" +
+                    "\nArtist(s): " + response.tracks.items[i].artists[0].name + 
+                    "\nSong Name: " + response.tracks.items[i].name +
+                    "\nAlbum Name: " + response.tracks.items[i].album.name +
+                    "\nPreview Link: " + response.tracks.items[i].preview_url;
+                    
+            console.log(spotifyResults);
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
 }
-function movieThis(searchTermHyphen) {
-  // setting search parameter to Transporter if undeclared
-  if (!searchTermHyphen) {
-    searchTermHyphen = "Transporter";
-  }
 
-  const URL = `http://www.omdbapi.com/?apikey=trilogy&t=${searchTermHyphen}`;
-  axios.get(URL).then(function(response) {
-    const answer = response.data;
-    console.log(answer.Title);
-    console.log(answer.Year);
-    console.log("IMDB rating: " + answer.Ratings[0].Value);
-    // console.log("Rotten Tomatoes rating: " + answer.Ratings[1].Value);
-    console.log(answer.Country);
-    console.log(answer.Language);
-    console.log(answer.Plot);
-    console.log(answer.Actors);
-  });
+function movieThis(value) {
+    if(!value){
+        value = "mr nobody";
+    }
+    axios.get("https://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy")
+    .then(function(response) {
+            var movieResults = 
+                "--------------------------------------------------------------------" +
+                    "\nMovie Title: " + response.data.Title + 
+                    "\nYear of Release: " + response.data.Year +
+                    "\nIMDB Rating: " + response.data.imdbRating +
+                    "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                    "\nCountry Produced: " + response.data.Country +
+                    "\nLanguage: " + response.data.Language +
+                    "\nPlot: " + response.data.Plot +
+                    "\nActors/Actresses: " + response.data.Actors;
+            console.log(movieResults);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    
 }
 
-if (process.argv[2] === "do-what-it-says") {
-  fs.readFile("random.txt", "utf8", (err, data) => {
-    if (err) throw err;
-    const dataArray = data.split(",");
-    if (dataArray[0] === "concert-this") {
-      concertThis(dataArray[1].replace(" ", "-"));
-    }
-    if (dataArray[0] === "spotify-this-song") {
-      spotifyThisSong(dataArray[1]);
-    }
-    if (dataArray[0] === "movie-this") {
-      movieThis(dataArray[1].replace(" ", "-"));
-    }
-  });
+function doThis(value) {
+
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        var dataArr = data.split(',');
+        spotifySong(dataArr[0], dataArr[1]);
+    })
 }
